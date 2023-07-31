982bf95bce18d9702ae7fc255d803e7bd7788471
Rocky <ec2-user@ip-172-31-8-132.ap-southeast-2.compute.internal>
2023-07-31 12:34:27 +0000

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 // Route::controller(AAAController::class)->group(function() {
 //     Route::get('XXX', 'bbb');
 // });
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
 });
+
 
 Auth::routes();
 
